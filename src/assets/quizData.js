545dc9626f2a8e291d216baf707bdbbc4188e399
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-09 14:04:21 +0500

    feat: add quiz functionality

---
+++ src/assets/quizData.js
@@ -0,0 +1,27 @@
+export const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Rome"],
+      answer: "Paris"
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Mars", "Jupiter", "Venus", "Saturn"],
+      answer: "Mars"
+    },
+    {
+      question: "What is the largest mammal in the world?",
+      options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+      answer: "Blue Whale"
+    },
+    {
+      question: "Who wrote 'Romeo and Juliet'?",
+      options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+      answer: "William Shakespeare"
+    },
+    {
+      question: "What is the powerhouse of the cell?",
+      options: ["Nucleus", "Mitochondria", "Endoplasmic Reticulum", "Golgi Apparatus"],
+      answer: "Mitochondria"
+    }
+  ];
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react";
+import { quizData } from "../../assets/quizData";
 import "./../../styles/Quiz/Quiz.css";
 
 const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(quizData[index]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [result, setResult] = useState(false);
+
+  const checkAns = (e, answer, isCorrect) => {
+    if (!lock) {
+      if (isCorrect) {
+        e.target.classList.add("correct");
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("wrong");
+      }
+    }
+    setLock(true);
+  };
+
+
+
   return (
     <div className="quiz container">
       <h2 className="quiz-title">Quiz App</h2>
       <hr />
-      <h3 className="quiz-question">Which?</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-      <button className="quiz-btn">Next</button>
-      <div className="quiz-index">1 of 5 questions</div>
+          <h3 className="quiz-question">
+            {index + 1}. {question.question}
+          </h3>
+          <ul className="quiz-list">
+            {question.options.map((option, index) => (
+              <li
+                key={index}
+                className="quiz-option"
+                onClick={(e) => {
+                  checkAns(e, option, option === question.answer);
+                }}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+          <button className="quiz-btn" >
+            NEXT
+          </button>
+          <div className="quiz-index">
+            {index + 1} of {quizData.length} questions
+          </div>
+
+
     </div>
   );
 };
