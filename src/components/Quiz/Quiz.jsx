fdd0681dae1e82fc0b626c0162b4fb6268d774b4
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-09 14:05:51 +0500

    feat: add next btn functionality

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -21,12 +21,33 @@ const Quiz = () => {
     setLock(true);
   };
 
-
+  const next = () => {
+    if (lock === true) {
+      if (index === quizData.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(quizData[index]);
+      setLock(false);
+      document.querySelectorAll(".quiz-option").forEach((option) => {
+        option.classList.remove("correct", "wrong");
+      });
+    }
+  };
 
   return (
     <div className="quiz container">
       <h2 className="quiz-title">Quiz App</h2>
       <hr />
+      {result ? (
+        <>
+          <h3 className="quiz-question">You Scored {score} out of {quizData.length}</h3>
+          <button className="quiz-btn">RESET</button>
+        </>
+      ) : (
+        <>
+          {" "}
           <h3 className="quiz-question">
             {index + 1}. {question.question}
           </h3>
@@ -43,14 +64,14 @@ const Quiz = () => {
               </li>
             ))}
           </ul>
-          <button className="quiz-btn" >
+          <button className="quiz-btn" onClick={next}>
             NEXT
           </button>
           <div className="quiz-index">
             {index + 1} of {quizData.length} questions
           </div>
-
-
+        </>
+      )}
     </div>
   );
 };
